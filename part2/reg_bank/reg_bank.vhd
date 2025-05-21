c3dfa86fd1dc060c1537c1b16ffba53f893b1752
VLtim43 <fernandoreal.valentim@gmail.com>
2025-05-20 21:07:33 -0300

    update

--- part2/reg_bank/reg_bank.vhd
+++ part2/reg_bank/reg_bank.vhd
@@ -5,13 +5,15 @@ USE IEEE.STD_LOGIC_1164.ALL;
 ENTITY reg_bank IS
     PORT (
         clk_in : IN STD_LOGIC; -- Clock Signal
-        nrst : IN STD_LOGIC; -- Reset
-        regn_di : IN STD_LOGIC_VECTOR(7 DOWNTO 0); -- Data Input
         regn_wr_ena : IN STD_LOGIC; -- Write Enable
 
-        regn_do : OUT STD_LOGIC_VECTOR(7 DOWNTO 0); -- Data Output
+        nrst : IN STD_LOGIC; -- Reset
+        regn_di : IN STD_LOGIC_VECTOR(7 DOWNTO 0); -- Data Input
+
         regn_wr_sel : IN STD_LOGIC_VECTOR(1 DOWNTO 0); -- Register Write Select
-        regn_rd_sel : IN STD_LOGIC_VECTOR(1 DOWNTO 0) -- Register Read Select
+        regn_rd_sel : IN STD_LOGIC_VECTOR(1 DOWNTO 0); -- Register Read Select
+
+        regn_do : OUT STD_LOGIC_VECTOR(7 DOWNTO 0) -- Data Output
 
         -- c_flag_in        : IN  STD_LOGIC; 
         -- z_flag_in        : IN  STD_LOGIC; 
@@ -28,30 +30,36 @@ END reg_bank;
 
 ARCHITECTURE Hardware OF reg_bank IS
     SIGNAL reg0 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0'); -- Initializes a 8bit register 
+    SIGNAL reg1 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0'); -- Initializes another 8bit register 
+    SIGNAL reg2 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0'); -- Initializes another 8bit register 
 BEGIN
     -- Writing Sequential  Process
     PROCESS (clk_in, nrst) -- Process triggers whenever clock changes, or it's reset
     BEGIN
-        IF nrst = '0' THEN
-            reg0 <= (OTHERS => '0'); -- If reset = 0, all bits of reg are cleared to 0000000
+        IF nrst = '0' THEN -- If reset = 0, all bits are cleated to 00000000
+            reg0 <= (OTHERS => '0');
+            reg1 <= (OTHERS => '0');
+            reg2 <= (OTHERS => '0');
+
         ELSIF rising_edge(clk_in) THEN -- Else If the clock is on rising edge
             IF regn_wr_ena = '1' THEN -- If writing is enable
                 IF regn_wr_sel = "00" THEN -- Select the 00 register for writing
                     reg0 <= regn_di;
+                ELSIF regn_wr_sel = "01" THEN -- Select the 01 register for writing
+                    reg1 <= regn_di;
+                ELSIF regn_wr_sel = "10" THEN -- Select the 10 register for writing
+                    reg2 <= regn_di;
                 END IF;
-                -- Other values (01, 10, 11) do nothing for now
+                -- Other value (11) does nothing for now
             END IF;
         END IF;
     END PROCESS;
 
-    -- Reading Combinational Process
-    PROCESS (regn_rd_sel, reg0) -- Given the resiter Selected
-    BEGIN
-        CASE regn_rd_sel IS -- Outputs the selected register value
-            WHEN "00" =>
-                regn_do <= reg0;
-            WHEN OTHERS =>
-                regn_do <= (OTHERS => '0');
-        END CASE;
-    END PROCESS;
+    -- Reading Combinational Assignment
+    WITH regn_rd_sel SELECT -- Outputs the selected register value
+        regn_do <=
+        reg0 WHEN "00",
+        reg1 WHEN "01",
+        reg2 WHEN "10",
+        (OTHERS => '0') WHEN OTHERS;
 END Hardware;
