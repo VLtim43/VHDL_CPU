4b4bc68ab8d420735bb1bce0ff4224c46b2af66a
VLtim43 <fernandoreal.valentim@gmail.com>
2025-06-04 21:03:25 -0300

    port_io

--- part3/port_io/port_io.vhd
+++ part3/port_io/port_io.vhd
@@ -3,9 +3,7 @@
 -- |    abus       | wr_en  | rd_en  |        operação        |
 -- |---------------|--------|--------|------------------------|
 -- | base_addr     |   1    |   0    | write on port_reg      |
--- | base_addr     |   0    |   1    | read on latch          |
--- | base_addr + 1 |   1    |   0    | write on dir_reg       |
--- | base_addr + 1 |   0    |   1    | read on dir_reg        |
+-- | base_addr     |   0    |   1    | read from port_reg     |
 -- ------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
@@ -16,15 +14,13 @@ ENTITY port_io IS
         base_addr : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0')
     );
     PORT (
-        clk_in  : IN  STD_LOGIC;
-        nrst    : IN  STD_LOGIC;
-
-        abus    : IN  STD_LOGIC_VECTOR(7 DOWNTO 0);
-        dbus    : IN  STD_LOGIC_VECTOR(7 DOWNTO 0);
-        wr_en   : IN  STD_LOGIC;
-        rd_en   : IN  STD_LOGIC;
-
-        port_reg_out : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)  
+        clk_in : IN STD_LOGIC;
+        nrst : IN STD_LOGIC;
+        abus : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+        dbus : INOUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+        wr_en : IN STD_LOGIC;
+        rd_en : IN STD_LOGIC;
+        port_reg_out : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
     );
 END ENTITY port_io;
 
@@ -32,16 +28,23 @@ ARCHITECTURE rtl OF port_io IS
     SIGNAL port_reg : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
 BEGIN
 
+    -- Synchronous write to port_reg
     PROCESS (clk_in, nrst)
     BEGIN
         IF nrst = '0' THEN
             port_reg <= (OTHERS => '0');
         ELSIF rising_edge(clk_in) THEN
-            IF wr_en = '1' AND abus = base_addr THEN
+            IF (wr_en = '1') AND (abus = base_addr) THEN
                 port_reg <= dbus;
             END IF;
         END IF;
     END PROCESS;
+
+    -- Read
+    dbus <= port_reg
+        WHEN (rd_en = '1' AND abus = base_addr)
+        ELSE
+        (OTHERS => 'Z');
 
     port_reg_out <= port_reg;
 
