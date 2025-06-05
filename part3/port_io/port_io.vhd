5e2d598e7486b12a58474c56da9c141f8400a34c
VLtim43 <fernandoreal.valentim@gmail.com>
2025-06-05 12:46:53 -0300

    uport-reg

--- part3/port_io/port_io.vhd
+++ part3/port_io/port_io.vhd
@@ -25,44 +25,55 @@ ENTITY port_io IS
         wr_en : IN STD_LOGIC; -- Write enable
         rd_en : IN STD_LOGIC; -- Read enable
 
-        port_reg_out : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
-        dir_reg_out : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
+        port_io : INOUT STD_LOGIC_VECTOR(7 DOWNTO 0) -- Bidirectional 8-bit I/O port
     );
 END ENTITY port_io;
 
 ARCHITECTURE rtl OF port_io IS
     SIGNAL port_reg : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
     SIGNAL dir_reg : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
-
+    SIGNAL latch : STD_LOGIC_VECTOR(7 DOWNTO 0);
     -- base_addr + 1
     CONSTANT base_addr_plus_one : STD_LOGIC_VECTOR(7 DOWNTO 0) :=
     STD_LOGIC_VECTOR(unsigned(base_addr) + 1);
 BEGIN
-
-    -- Synchronous writes to port_reg and dir_reg 
+    gen_port_dir : FOR i IN 0 TO 7 GENERATE
+        port_io(i) <= port_reg(i) WHEN dir_reg(i) = '1' ELSE
+        'Z';
+    END GENERATE;
+    -- Synchronous writes to port_reg and dir_reg
     PROCESS (clk_in, nrst)
     BEGIN
         IF nrst = '0' THEN
             port_reg <= (OTHERS => '0');
             dir_reg <= (OTHERS => '0');
+            latch <= (OTHERS => '0');
         ELSIF rising_edge(clk_in) THEN
             IF (wr_en = '1') THEN
-                IF (abus = base_addr) THEN -- Write into port_reg
+                IF (abus = base_addr) THEN
                     port_reg <= dbus;
-                ELSIF (abus = base_addr_plus_one) THEN -- Write into dir_reg
+                ELSIF (abus = base_addr_plus_one) THEN
                     dir_reg <= dbus;
                 END IF;
+            END IF;
+
+            IF (rd_en = '1' AND abus = base_addr) THEN
+                FOR i IN 0 TO 7 LOOP
+                    IF dir_reg(i) = '0' THEN
+                        latch(i) <= port_io(i); -- Capture input
+                    ELSE
+                        latch(i) <= port_reg(i); -- Output mode: reflect port_reg
+                    END IF;
+                END LOOP;
             END IF;
         END IF;
     END PROCESS;
 
-    -- Combinational read 
-    dbus <= port_reg
+    -- Combinational read
+    dbus <= latch
         WHEN (rd_en = '1' AND abus = base_addr) ELSE
         dir_reg
         WHEN (rd_en = '1' AND abus = base_addr_plus_one) ELSE
         (OTHERS => 'Z');
-    port_reg_out <= port_reg;
-    dir_reg_out <= dir_reg;
 
 END ARCHITECTURE rtl;
